791953a518aba97a802c0d518799365d5f5aa677
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-03-19 22:19:13 +0100

    back

--- src/android/Wdgt.java
+++ src/android/Wdgt.java
@@ -11,8 +11,6 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.RemoteViews;
 
-import android.content.res.Resources;
-
 public class Wdgt extends AppWidgetProvider {
 
   private static final String ACTION_CLICK = "ACTION_CLICK";
@@ -29,16 +27,11 @@ public class Wdgt extends AppWidgetProvider {
       // create some random data
       int number = (new Random().nextInt(100));
 
-      String package_name = getApplication().getPackageName();
-      Resources resources = getApplication().getResources();
-
       RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
-          resources.getIdentifier("widget_layout", "layout", package_name));
-          //R.layout.widget_layout);
+          R.layout.widget_layout);
       Log.w("WidgetExample", String.valueOf(number));
       // Set the text
-      //remoteViews.setTextViewText(R.id.update, String.valueOf(number));
-      remoteViews.setTextViewText(resources.getIdentifier("update", "id", package_name), String.valueOf(number));
+      remoteViews.setTextViewText(R.id.update, String.valueOf(number));
 
       // Register an onClickListener
       Intent intent = new Intent(context, Wdgt.class);
@@ -48,8 +41,7 @@ public class Wdgt extends AppWidgetProvider {
 
       PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
           0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-      //remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
-      remoteViews.setOnClickPendingIntent(resources.getIdentifier("update", "id", package_name), pendingIntent);
+      remoteViews.setOnClickPendingIntent(R.id.update, pendingIntent);
       appWidgetManager.updateAppWidget(widgetId, remoteViews);
     }
   }
