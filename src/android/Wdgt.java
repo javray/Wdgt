899f5685b5968ebc22ad46768210486615c5d01e
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-03-21 00:00:24 +0100

    listview

--- src/android/Wdgt.java
+++ src/android/Wdgt.java
@@ -57,7 +57,7 @@ public class Wdgt extends AppWidgetProvider {
 
       Log.v("Wdgt", Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)).toString());
 
-      remoteViews.setRemoteAdapter(widgetId, svcIntent);
+      remoteViews.setRemoteAdapter(widgetId, R.id.datos, svcIntent);
 
       remoteViews.setTextViewText(R.id.nombre, nombre);
       remoteViews.setImageViewUri(R.id.foto, foto);
