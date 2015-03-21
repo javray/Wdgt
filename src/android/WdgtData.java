37450aff2c18dd47094fdb1ff41955a461274576
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-03-21 23:51:57 +0100

    rows

--- src/android/WdgtData.java
+++ src/android/WdgtData.java
@@ -143,6 +143,8 @@ public class WdgtData implements RemoteViewsService.RemoteViewsFactory {
     catch(Exception e) {
     }
 
+    sData.add(new DataElement("Discapacidad", 1));
+
     Log.v("WdgtData", Integer.toString(sData.size()));
   }
 
