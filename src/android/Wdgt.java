1a0d31ece14ba278927c76edb3c99fc2740fd3bb
Fco. Javier Martín Carrasco <javray@gmail.com>
2015-03-23 13:04:08 +0100

    refresh

--- src/android/Wdgt.java
+++ src/android/Wdgt.java
@@ -47,11 +47,9 @@ public class Wdgt extends AppWidgetProvider {
       intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
       intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
 
-      /*
       PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
           0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-      remoteViews.setOnClickPendingIntent(R.id.layout, pendingIntent);
-      */
+      remoteViews.setOnClickPendingIntent(R.id.refrescar, pendingIntent);
 
       appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.datos);
       appWidgetManager.updateAppWidget(widgetId, remoteViews);
